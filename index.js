d25fba2c68ad824d4ef8f6e816a0c7d8e8cd0187
JacksonWeudes <weudes2024jackson@gmail.com>
2025-09-07 09:36:49 -0300

    sticky hide menu effect

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
+const body = document.body
+
 function initSideMenu()
 {
     const menuBtn = document.querySelector(".mobile-icon")
     const sideMenu = document.querySelector(".side-menu-nav")
+    const overlay = document.querySelector(".overlay")
 
     menuBtn.addEventListener("click", () => {
-        sideMenu.classList.add("active")
+        menuBtn.classList.toggle("active")
+        sideMenu.classList.toggle("active")
+        overlay.classList.toggle("active")
+        body.classList.toggle("active")
     })
 }
 
-initSideMenu()
+initHideOnScroll()
+{
+    let lastScroll = 0
+
+    window.addEventListener("scroll", () => {
+        const currentScroll = window.pageYOffset
+
+        if(currentScroll <= 0){
+            body.classList.remove("scroll-up")
+        }
+
+        if(currentScroll > lastScroll && !body.classList.contains("scroll-down")){
+            body.classList.remove("scroll-up")
+            body.classList.add("scroll-down")
+        }
+
+        if(currentScroll < lastScroll && body.classList.contains("scroll-down")){
+            body.classList.add("scroll-up")
+            body.classList.remove("scroll-down")
+        }
+
+        lastScroll = currentScroll
+    })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    initSideMenu()
+    initHideOnScroll()
+})
